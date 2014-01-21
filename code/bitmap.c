c88f653457a3c0e9664edd04b8e4e1e12457fc93
lixuze <lixuze@iyuewe.com>
2014-01-21 12:59:25 +0800

    更改函数名称 ， 使得 数组返回有目标

--- code/bitmap.c
+++ code/bitmap.c
@@ -2,9 +2,10 @@
 #include<stdlib.h>
 
 const int BUFFER_SIZE = 1000 ;
+
 const int MAX_VALUE = BUFFER_SIZE * 8;
 void * get_char_buffer(int size);
-int * getbyte(char * buffer , int size);
+int * getbyte(char * buffer , int buffer_size, const int size);
 void setbyte(char * buffer , int num);
 const int BYTE_ARRY[] = {1<<0,1<<2,1<<3,1<<4,1<<5,1<<6,1<<7};
 
@@ -12,7 +13,7 @@ const int BYTE_ARRY[] = {1<<0,1<<2,1<<3,1<<4,1<<5,1<<6,1<<7};
 
 int main(void)
 {
-    char *buffer = (char *)get_char_buffer(1000);
+    char *buffer = (char *)get_char_buffer(BUFFER_SIZE);
     if(buffer == NULL)
     {
         exit(0);
@@ -25,8 +26,8 @@ int main(void)
     setbyte(buffer , 1001);
     setbyte(buffer , 1234);
     setbyte(buffer, 1024);
-    int * arry  = getbyte(buffer , 8);
-    for(int i = 0 ; i < 6 ; i++)
+    int * arry  = getbyte(buffer , BUFFER_SIZE, 9 );
+    for(int i = 0 ; i < 9 ; i++)
     {
         printf("%d \n" , *(arry+i));
     }
@@ -35,11 +36,11 @@ int main(void)
 }
 
 
-int * getbyte(char * buffer , int size)
+int * getbyte(char * buffer , int buffer_size, const int size)
 {
 
     int * arry = (int *)get_char_buffer(size * 4);
-    int index = 999;
+    int index = buffer_size - 1;
     int byteIndex;
     int arryIndex = 0;
     for(;index >= 0 ; index--)
@@ -51,6 +52,10 @@ int * getbyte(char * buffer , int size)
             {
                 *(arry + arryIndex) = index * 8 + byteIndex;
                 arryIndex = arryIndex + 1;
+                if(arryIndex == size)
+                {
+                    return arry ;
+                }
             }
         }
     }
@@ -60,9 +65,13 @@ int * getbyte(char * buffer , int size)
 
 void setbyte(char * buffer , int num)
 {
+    if(num > MAX_VALUE)
+    {
+        printf("num must be < %d" , MAX_VALUE);
+        return;
+    }
     int high = num / 8 ;
     int low = num % 8;
-    printf("num %d =>high :%d low : %d \n" , num , high,low);
     *(buffer + high) = *(buffer + high) +  BYTE_ARRY[low] ;
 }
 
