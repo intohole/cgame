1d8f57ae4fae7cd2976f30cd6c870a553110fbb1
lixuze <intoblack86@gmail.com>
2014-01-21 14:10:53 +0800

    改变 bitmap 为struct 方便操作

--- code/bitmap.c
+++ code/bitmap.c
@@ -2,39 +2,103 @@
 #include<stdlib.h>
 
 const int BUFFER_SIZE = 1000 ;
-
 const int MAX_VALUE = BUFFER_SIZE * 8;
+const int BYTE_WIDTH = 8;
+
+typedef struct BIT_MAP
+{
+    char *buffer;
+    int size;
+    long MAX_VALUE;
+}BITMAP , *PBMAP;
+
 void * get_char_buffer(int size);
-int * getbyte(char * buffer , int buffer_size, const int size);
-void setbyte(char * buffer , int num);
+int * getbyte(char * buffer , const int buffer_size, const int size);
+void setbyte(char * buffer , const int num);
+PBMAP createBitMap(const int size);
+bool setBitMap(PBMAP bitMap , const int num);
+bool setbuffer(void * buffer ,const int buffersize, const char num);
+int *getByteMap(PBMAP bitMap , const int size);
 const int BYTE_ARRY[] = {1<<0,1<<2,1<<3,1<<4,1<<5,1<<6,1<<7};
+
+
+
 
 
 
 int main(void)
 {
-    char *buffer = (char *)get_char_buffer(BUFFER_SIZE);
-    if(buffer == NULL)
-    {
-        exit(0);
-    }
-    setbyte(buffer , 7);
-    setbyte(buffer , 2 );
-    setbyte(buffer , 101);
-    setbyte(buffer , 1);
-    setbyte(buffer , 5);
-    setbyte(buffer , 1001);
-    setbyte(buffer , 1234);
-    setbyte(buffer, 1024);
-    int * arry  = getbyte(buffer , BUFFER_SIZE, 9 );
+    PBMAP bitmap = createBitMap(1000);
+    setBitMap(bitmap , 7);
+    setBitMap(bitmap,  2);
+    setBitMap(bitmap , 101);
+    setBitMap(bitmap , 1);
+    setBitMap(bitmap , 5);
+    setBitMap(bitmap , 1001);
+    setBitMap(bitmap , 1234);
+    setBitMap(bitmap, 1024);
+    int * arry  = getByteMap(bitmap, 9 );
     for(int i = 0 ; i < 9 ; i++)
     {
         printf("%d \n" , *(arry+i));
     }
-    printf("%s" , buffer);
     return 0;
 }
 
+PBMAP createBitMap(const int size)
+{
+    PBMAP bitMap = (PBMAP)get_char_buffer(sizeof(BITMAP));
+    if(bitMap != NULL)
+    {
+        bitMap->buffer = (char *)get_char_buffer(size);
+        if(bitMap->buffer == NULL)
+        {
+            return NULL;
+        }
+        setbuffer((void *)bitMap->buffer , size , 0);
+        bitMap->size = size;
+        bitMap->MAX_VALUE =size * BYTE_WIDTH;
+    }
+    return bitMap;
+}
+
+
+bool setBitMap(PBMAP bitMap ,const int num)
+{
+    if(bitMap != NULL && (*bitMap).MAX_VALUE >= num)
+    {
+        bitMap->buffer[num / 8] = bitMap->buffer[num / 8] +  BYTE_ARRY[num % 8];
+        return true;
+    }
+    return false;
+}
+
+
+int *getByteMap(PBMAP bitMap , const int size)
+{
+
+    int * arry = (int *)get_char_buffer(size * 4);
+    int index = bitMap->size - 1;
+    int arryIndex = 0;
+    int byteIndex = 0;
+    for( ;index >=0 ; index-- )
+    {
+        byteIndex = 7;
+        for(;byteIndex >= 0 ; byteIndex--)
+        {
+            if(bitMap->buffer[index] & BYTE_ARRY[byteIndex])
+            {
+                *(arry + arryIndex) = index * 8 + byteIndex;
+                arryIndex = arryIndex + 1;
+                if(arryIndex == size)
+                {
+                    return arry ;
+                }
+            }
+        }
+    }
+    return arry;
+}
 
 int * getbyte(char * buffer , int buffer_size, const int size)
 {
@@ -43,6 +107,7 @@ int * getbyte(char * buffer , int buffer_size, const int size)
     int index = buffer_size - 1;
     int byteIndex;
     int arryIndex = 0;
+    setbuffer((void *)arry  , size * 4 , 0);
     for(;index >= 0 ; index--)
     {
         byteIndex = 7;
@@ -63,7 +128,7 @@ int * getbyte(char * buffer , int buffer_size, const int size)
 }
 
 
-void setbyte(char * buffer , int num)
+void setbyte(char * buffer , const int num)
 {
     if(num > MAX_VALUE)
     {
@@ -79,4 +144,21 @@ void setbyte(char * buffer , int num)
 void * get_char_buffer(int size)
 {
     return malloc(size);
+}
+
+
+bool setbuffer(void * buffer ,const int buffersize, const char num)
+{
+    int index = 0;
+    char *pChar = (char *)buffer;
+    if(pChar != NULL)
+    {
+        for( ; index < buffersize ; index++)
+        {
+            pChar[index] = num;
+        }
+        return true;
+    }
+    return false;
+
 }
